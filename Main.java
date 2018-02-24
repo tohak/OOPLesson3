12d983f1ff78a50649da4bab1f2d62ed121d7e10
mr.Towa <36159686+tohak@users.noreply.github.com>
2018-02-24 13:53:51 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,44 @@
+package com.konovalov;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Human humanOne= new Human("Konovalov", "Anton", 1991, true);
+		Student studentOne= new Student("One", "Arst", 1992, true, "It", 3);
+		Student studentTho= new Student("Tho", "Adsart", 1993, true, "It", 3);
+		Student studentThree= new Student("Three", "Adrt", 1994, true, "It", 3);
+		Student studentFour= new Student("Four", "Agfrt", 1995, true, "It", 3);
+		Student studentFive= new Student("Five", "Asrt", 1996, true, "It", 3);
+		Student studentSix= new Student("Six", "Arrt", 1997, false, "It", 3);
+		Student studentSeven= new Student("Seven", "Aeqrt", 1998, true, "It", 3);
+		Student studentEight= new Student("Eight", "Aeqwrt", 1999, true, "It", 3);
+		Student studentNine= new Student("Nine", "Areqt", 1992, false, "It", 3);
+		Student studentTen= new Student("Ten", "Aeqrt", 1993, true, "It", 3);
+		Student studentEleven= new Student("Eleven", "Aehrt", 1991, true, "It", 3);
+		
+		Group group=new Group();
+		
+		group.add(studentOne);
+		group.add(studentTho);
+		group.add(studentThree);
+		group.add(studentFour);
+		group.add(studentFive);
+		group.add(studentSix);
+		group.add(studentSeven);
+		group.add(studentEight);
+		group.add(studentNine);
+		group.add(studentTen);
+		//group.add(studentEleven);
+		
+		group.delete(studentSix);
+		 
+		System.out.println(humanOne);
+		System.out.println(studentOne);
+		System.out.println(group);
+		System.out.println();
+		System.out.println(group.search("Seven").toString());
+		
+	}
+
+}
